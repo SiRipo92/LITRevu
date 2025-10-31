e9416c7bfbf6cd973e34251336e5ebd724aa679e
SiRipo92 <ripochesierra@gmail.com>
2025-10-31 11:48:35 +0100

    Switched to nose testing and coverage and added pipeline

--- LITRevu/settings.py
+++ LITRevu/settings.py
@@ -133,3 +133,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL = "home"
 LOGIN_REDIRECT_URL = "feed"
 LOGOUT_REDIRECT_URL = "home"
+
+# Testing
+TEST_RUNNER = "green.djangorunner.DjangoRunner"
