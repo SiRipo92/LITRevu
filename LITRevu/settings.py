ea2ed6c7a2e1989d13ab0260f1e344bc5a12fa95
SiRipo92 <ripochesierra@gmail.com>
2025-11-28 22:49:46 +0100

    Removed 0.0.0.0 allowed_host

--- LITRevu/settings.py
+++ LITRevu/settings.py
@@ -34,7 +34,6 @@ DEBUG = os.getenv("DJANGO_DEBUG", "1") == "1"
 ALLOWED_HOSTS = os.environ.get(
     "DJANGO_ALLOWED_HOSTS",
     "localhost,127.0.0.1",
-    "0.0.0.0"
 ).split(",")
 
 CSRF_TRUSTED_ORIGINS = [
